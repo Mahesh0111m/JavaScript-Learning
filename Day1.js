345030d71eb5643d6dce98c63211e72f23c07305
Mahesh0111m <maheshm0111mhm@gmail.com>
2024-12-27 23:18:57 +0530

    JavascriptDay1

---
+++ Day1.js
@@ -0,0 +1,9 @@
+//Slice 
+//Syntax slice(x,y) 
+// x included y excluded
+
+var text = "hello";
+console.log(text.slice(0,3));
+
+var tweet = "djjjjjjjjjjjeeeeeeeeeeeeeeeeeeefffffffffffffffffffffddddddddddddd";
+console.log(tweet.slice(0,20));
